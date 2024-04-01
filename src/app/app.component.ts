aea3c4da7ff7da2866dc7002c69150062732a35f
Gabriella Alves de Oliveira <oliveira.gabriellaalves@gmail.com>
2024-04-01 12:15:43 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { ProductAlertsComponent } from "./components/product-alerts/product-alerts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, ProductListComponent, ProductAlertsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
