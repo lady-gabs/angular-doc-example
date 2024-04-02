e63c3ba947609d104af4fec3552c85dc0292fb54
Gabriella Alves de Oliveira <oliveira.gabriellaalves@gmail.com>
2024-04-02 09:32:07 -0300

    exercise finished

--- src/app/components/product-alerts/product-alerts.component.ts
+++ src/app/components/product-alerts/product-alerts.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../products';
 
 @Component({
   selector: 'app-product-alerts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-alerts.component.html',
   styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
-
+  @Input() cellProduct: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { productsInfos } from '../../products';
+import { ProductAlertsComponent } from "../product-alerts/product-alerts.component";
 
 @Component({
-  selector: 'app-product-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+    selector: 'app-product-list',
+    standalone: true,
+    templateUrl: './product-list.component.html',
+    styleUrl: './product-list.component.css',
+    imports: [CommonModule, ProductAlertsComponent]
 })
 export class ProductListComponent {
+  products = [...productsInfos]; 
 
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+  
 }
